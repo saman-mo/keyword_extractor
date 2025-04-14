2e9616e6cd5549b98a27c2e6d15683c086b57955
saman-mo <saman.moein81@gmail.com>
2025-04-14 15:00:29 +0200

    updated config.js to handle different envs

--- app/static/config.js
+++ app/static/config.js
@@ -1,3 +1,7 @@
+const isProduction = window.location.hostname.includes("saman-moeinsadat-ai-lab.de");
+
 window.CONFIG = {
-    BACKEND_URL: "http://127.0.0.1:8000"
+    BACKEND_URL: isProduction
+                ?  "https://keyword-extractor.saman-moeinsadat-ai-lab.de" 
+                :  "http://127.0.0.1:8000"
   };
